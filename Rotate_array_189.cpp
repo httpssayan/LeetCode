f4965a34319a0ea2320a01dd257375f3bad61857
httpssayan <sayan5564jana@gmail.com>
2026-01-06 02:06:44 +0530

    Add files via upload

---
+++ Rotate_array_189.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void ReverseArray(vector<int>& nums,int start, int end){
+        while(start<end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        ReverseArray(nums,0,n-1);
+        ReverseArray(nums,0,k-1);
+        ReverseArray(nums,k,n-1);
+    }
+};
